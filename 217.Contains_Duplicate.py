5e35d5c0a4197356e2e1b1e7260a2e75f0278d20
Rafi <rafitanjibulhasan@gmail.com>
2022-01-08 19:32:03 +0600

    contains_duplicate

---
+++ 217.Contains_Duplicate.py
@@ -0,0 +1,17 @@
+class Solution:
+    def containsDuplicate(nums) -> bool:
+        table = set()
+        for i in nums:
+            if i in table:
+                return True
+            else:
+                table.add(i)
+        return False
+
+# if __name__ == "__main__":
+    
+#     List = [10, 5, 3, 4, 4, 6]
+#     if (Solution.containsDuplicate(List)):
+#         print("True")
+#     else:
+#         print("False")
