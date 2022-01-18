1183a0ce01e282a528435413d01116763a54d60c
Tanjibul Hasan Rafi <rafitanjibulhasan@gmail.com>
2022-01-19 02:38:17 +0600

    Update 136.Single_Number.py

--- 136.Single_Number.py
+++ 136.Single_Number.py
@@ -1,3 +1,5 @@
+#First way::
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         hash_table = {}
@@ -6,4 +8,15 @@ class Solution:
                 hash_table.pop(i)
             except:
                 hash_table[i] = 1
-        return hash_table.popitem()[0]# popitem(): Remove the last item from the dictionary:
+        return hash_table.popitem()[0]# popitem(): Remove the last item from the dictionary
+    
+#Second way::
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        i = 0
+        while i < len(nums):
+                if nums[i] not in nums[i+1:]:
+                    return nums[i]
+                else:
+                    i += 2
