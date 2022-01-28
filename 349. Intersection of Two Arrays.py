e290907d03c33e44a5d2b6e8ea5ccf219c64ca1b
Tanjibul Hasan Rafi <rafitanjibulhasan@gmail.com>
2022-01-28 18:37:03 +0600

    optimal solution

--- 349. Intersection of Two Arrays.py
+++ 349. Intersection of Two Arrays.py
@@ -10,3 +10,13 @@ class Solution:
                 a_set.add(i)
                 
         return a_set
+
+
+#second solution:
+
+# class Solution:
+#     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+#         a_set = set(nums1) & set(nums2)
+
+#         return a_set
