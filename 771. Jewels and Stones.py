4b0e0a48e493006e858dda3e0e1f84cabf2b0b9e
Tanjibul Hasan Rafi <rafitanjibulhasan@gmail.com>
2022-02-12 03:10:01 +0600

    Update 771. Jewels and Stones.py

--- 771. Jewels and Stones.py
+++ 771. Jewels and Stones.py
@@ -6,3 +6,11 @@ class Solution:
             if stones[i] in a_set:
                 count+=1
         return count
+
+#second solution:
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        for s in jewels:
+            count += stones.count(s) #at first checking how many a in stones jewels = "aA", stones = "aAAbbbb"
+        return count
