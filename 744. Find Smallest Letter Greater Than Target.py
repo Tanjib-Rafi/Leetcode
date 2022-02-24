b33751fe46de661f7070f72c6c40fa96f5cefca6
Tanjibul Hasan Rafi <rafitanjibulhasan@gmail.com>
2022-02-25 00:45:17 +0600

    binary search

--- 744. Find Smallest Letter Greater Than Target.py
+++ 744. Find Smallest Letter Greater Than Target.py
@@ -3,4 +3,23 @@ class Solution:
         for i in letters:
             if ord(target) < ord(i):
                 return i
-        return letters[0]     
+        return letters[0]   
+    
+#second solution(binary search):
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        a = letters
+        low = 0
+        high = len(a)-1
+        ans = a[0]
+        while(low<=high):
+            mid = (low+high)//2
+            if(a[mid]==target):
+                low = mid+1
+            elif(a[mid] > target):
+                ans = a[mid]
+                high = mid-1
+            elif(a[mid] < target):
+                low = mid+1
+        return ans
